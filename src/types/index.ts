caf65dce2177ef7235d66cb5d725473405dd0988
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-04-20 15:44:59 +0600

    Export Data Types

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export { ArtistNameType, SongNameType, TrackIDType } from "./paramTypes";
+export {
+  LyricsReturnType,
+  MacroLyricsReturnType,
+  SubtitleReturnType,
+  TrackIDReturnType,
+  TranslationReturnType,
+} from "./returnTypes";
