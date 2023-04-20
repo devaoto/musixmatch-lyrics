06a882b16132876d7f85063f37fd3c194352469f
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-04-20 15:44:47 +0600

    Create custom Parameter Type

---
+++ src/types/paramTypes.ts
@@ -0,0 +1,6 @@
+import { MusixmatchString } from "./rawDataTypes";
+type TrackIDType = MusixmatchString;
+type ArtistNameType = MusixmatchString;
+type SongNameType = MusixmatchString;
+
+export { TrackIDType, ArtistNameType, SongNameType };
